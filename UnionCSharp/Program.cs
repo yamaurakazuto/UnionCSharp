010c0d7f96e6fdb8f2e32c62fc0d11156f968c79
Kazuto Yamaura <xyst510@icloud.com>
2026-06-13 15:07:58 +0900

    first commit

---
+++ UnionCSharp/Program.cs
@@ -0,0 +1,20 @@
+﻿public record class Success(string Message);
+public record class Error(string Message);
+
+public union Result(Success, Error);
+
+public class Program
+{
+    public static void Main()
+    {
+        Result result = new Success("保存成功");
+
+        string message = result switch
+        {
+            Success(var msg) => $"OK: {msg}",
+            Error(var msg) => $"NG: {msg}"
+        };
+
+        Console.WriteLine(message);
+    }
+}
